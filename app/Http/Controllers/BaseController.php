060d538b9458f4d5985a3e16b13a1c83f6eced44
huythq <huythq@omt.vn>
2024-09-26 22:18:19 +0700

    source

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -4,5 +4,13 @@ namespace App\Http\Controllers;
 
 class BaseController extends Controller
 {
+    public function __construct()
+    {
+    }
+    public function responseSuccess($data = [], $message = 'success')
+    {
+        return response()->json([
 
+        ]);
+    }
 }
