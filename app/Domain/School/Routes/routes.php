83ba157ea461ff90a4234070dde69a828b8bb6ff
xuanhoang2004 <xuanhoangg2004@gmail.com>
2024-09-29 15:33:04 +0700

    update check auth api router

--- app/Domain/School/Routes/routes.php
+++ app/Domain/School/Routes/routes.php
@@ -2,8 +2,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Domain\School\Controllers\SchoolController;
+use GuzzleHttp\Middleware;
 
-Route::group(['prefix' => 'manager/school'], function () {
+Route::group(['prefix' => 'manager/school', 'middleware' => 'auth:api'], function () {
     // Route để lấy danh sách trường học
     Route::get('/', [SchoolController::class, 'index']);
     
