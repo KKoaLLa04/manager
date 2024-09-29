dc5e71b122cebd47910207cf20668730a9539792
TheBao2004 <nguyentheb705@gmail.com>
2024-09-29 15:58:00 +0700

    Day code them seeder cho school_year

--- resources/lang/vi/api.php
+++ resources/lang/vi/api.php
@@ -7,4 +7,7 @@ return [
         'school_has_not_module' => 'Trường của bé chưa được sử dụng tính năng này',
         'user_not_permission' => 'Người dùng không có quyền truy cập'
     ],
+    'school_year' => [
+        'start_date_before_end_date' => 'Thời gian bắt đầu phải lớn hơn hoặc bằng thời gian hiện tại',
+    ]
 ];
