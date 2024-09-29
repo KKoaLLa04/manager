3f7c7fc4c91d2f64a10e174f3b6cce39313b9e51
xuanhoang2004 <xuanhoangg2004@gmail.com>
2024-09-29 15:56:14 +0700

    update leng

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -7,46 +7,15 @@ return [
         'school_has_not_module' => 'Your school does not have this feature yet.',
         'user_not_permission' => 'User does not have access',
         
-
-        'required' => 'The school code field is required.',
-        'string' => 'The school code must be a string.',
-        'min' => 'The school code must be at least :min characters.',
-        'max' => 'The school code must not exceed :max characters.',
-        'unique' => 'This school code already exists, please choose another code.',
-
-        // Messages for the 'name' field
-        'required' => 'The school name field is required.',
-        'string' => 'The school name must be a string.',
-        'min' => 'The school name must be at least :min characters.',
-        'max' => 'The school name must not exceed :max characters.',
-
-        // Messages for the 'avatar' field
-        'url' => 'The avatar URL is invalid.',
-        'image' => 'The avatar must be an image file.',
-        'mimes' => 'The avatar must be in one of the following formats: jpeg, png, jpg, or gif.',
-        'max' => 'The avatar must not exceed 2MB in size.',
-
-        // Messages for the 'address' field
-        'required' => 'The address field is required.',
-        'string' => 'The address must be a string.',
-        'max' => 'The address must not exceed :max characters.',
-
-        // Messages for the 'logo' field
-        'url' => 'The logo URL is invalid.',
-        'image' => 'The logo must be an image file.',
-        'mimes' => 'The logo must be in one of the following formats: jpeg, png, jpg, or gif.',
-        'max' => 'The logo must not exceed 2MB in size.',
-
-        // Messages for the 'telephone' field
-        'required' => 'The telephone number is required.',
+        'required' => '  field is required.',
+        'string' => '  must be a string.',
+        'min' => '  must be at least :min characters.',
+        'max' => '  must not exceed :max characters.',
+        'unique' => '  already exists, please choose another code.',
+        'url' => 'The URL is invalid.',
+        'image' => 'The must be an image file.',
+        'mimes' => 'The must be in one of the following formats: jpeg, png, jpg, or gif.',
         'regex' => 'The telephone number must be 10-11 digits long and contain only numbers.',
-
-        // Messages for the 'email' field
-        'required' => 'The email field is required.',
-        'unique' => 'This email already exists, please choose another one.',
-
-        // Messages for the 'modified_user_id' field
-        'modified_user_id.required' => 'The modified user is required.',
 
     ],
     
--- resources/lang/vi/api.php
+++ resources/lang/vi/api.php
@@ -7,45 +7,15 @@ return [
         'school_has_not_module' => 'Trường của bé chưa được sử dụng tính năng này',
         'user_not_permission' => 'Người dùng không có quyền truy cập',
         
+        'required' => ' trường này là bắt buộc.',
+        'string' => ' phải là một chuỗi ký tự.',
+        'min' => ' phải có ít nhất :min ký tự.',
+        'max' => ' không được vượt quá :max ký tự.',
+        'unique' => ' đã tồn tại, vui lòng chọn mã khác.',
+        'url' => 'URL không hợp lệ.',
+        'image' => ' phải là một tệp hình ảnh.',
+        'mimes' => ' phải ở một trong các định dạng sau: jpeg, png, jpg, hoặc gif.',
+        'regex' => 'Số điện thoại phải có độ dài từ 10-11 chữ số và chỉ chứa số.'
 
-        'required' => 'Trường mã trường học là bắt buộc.',
-        'string' => 'Mã trường học phải là chuỗi ký tự.',
-        'min' => 'Mã trường học phải có ít nhất :min ký tự.',
-        'max' => 'Mã trường học không được vượt quá :max ký tự.',
-        'unique' => 'Mã trường học này đã tồn tại, vui lòng chọn mã khác.',
-        
-         // Messages cho trường 'name'
-         'required' => 'Trường tên trường học là bắt buộc.',
-         'string' => 'Tên trường học phải là chuỗi ký tự.',
-         'min' => 'Tên trường học phải có ít nhất :min ký tự.',
-         'max' => 'Tên trường học không được vượt quá :max ký tự.',
-
-         // Messages cho trường 'avatar'
-         'url' => 'URL của avatar không hợp lệ.',
-         'image' => 'Avatar phải là một tệp hình ảnh.',
-         'mimes' => 'Avatar phải có định dạng: jpeg, png, jpg, hoặc gif.',
-         'max' => 'Kích thước avatar không được vượt quá 2MB.',
-
-         // Messages cho trường 'address'
-         'required' => 'Địa chỉ là bắt buộc.',
-         'string' => 'Địa chỉ phải là chuỗi ký tự.',
-         'max' => 'Địa chỉ không được vượt quá :max ký tự.',
-
-         // Messages cho trường 'logo'
-         'url' => 'URL của logo không hợp lệ.',
-         'image' => 'Logo phải là một tệp hình ảnh.',
-         'mimes' => 'Logo phải có định dạng: jpeg, png, jpg, hoặc gif.',
-         'max' => 'Kích thước logo không được vượt quá 2MB.',
-
-         // Messages cho trường 'telephone'
-         'required' => 'Số điện thoại là bắt buộc.',
-         'regex' => 'Số điện thoại phải có độ dài 10-11 chữ số và chỉ chứa số.',
-
-         // Messages cho trường 'email'
-         'required' => 'Email là bắt buộc.',
-         'unique' => 'Email này đã tồn tại, vui lòng chọn email khác.',
-
-         // Messages cho trường 'modified_user_id'
-         'modified_user_id.required' => 'Người dùng chỉnh sửa là bắt buộc.',
     ],
 ];
