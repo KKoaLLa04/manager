ef7e2ef3cb12161e7dceec0e0e6e92d5dab8a8dd
BigH0405 <khanhbeotixiu8>
2024-09-29 09:37:59 +0700

    Code crud va check user

--- app/Domain/AcademicYear/Controllers/AcademicYearController.php
+++ app/Domain/AcademicYear/Controllers/AcademicYearController.php
@@ -2,25 +2,36 @@
 
 namespace App\Domain\AcademicYear\Controllers;
 
+use App\Common\Enums\AccessTypeEnum;
+use App\Common\Repository\GetUserRepository;
 use App\Domain\AcademicYear\Models\AcademicYear;
 use App\Domain\AcademicYear\Repository\AcademicYearReposity;
 use App\Domain\AcademicYear\Requests\AcademicYearRequest;
 use App\Http\Controllers\BaseController;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Enum;
 
 class AcademicYearController extends BaseController
 {
     protected $academicYearRepository;
+    
 
     public function __construct(AcademicYearReposity $academicYearRepository)
     {
         $this->academicYearRepository = $academicYearRepository;
+        
     }
 
     
-    public function index()
+    public function index(Request $request, GetUserRepository $getUserRepository)
     {
+        $user_id = $request->user_id;
+        $type = AccessTypeEnum::MANAGER->value;
         
+        $getUser = $getUserRepository->getUser($user_id, $type); 
+        if (!$getUser) {
+            return $this->responseError(trans('api.error.user_not_permission'));
+        }
         $academicYears = $this->academicYearRepository->getAcademicYear();
         
         
@@ -30,7 +41,14 @@ class AcademicYearController extends BaseController
         ], 200);
     }
 
-    public function show($id){
+    public function show(int $id, Request $request, GetUserRepository $getUserRepository){
+        $user_id = $request->user_id;
+        $type = AccessTypeEnum::MANAGER->value;
+        
+        $showUser = $getUserRepository->getUser($user_id, $type); 
+        if (!$showUser) {
+            return $this->responseError(trans('api.error.user_not_permission'));
+        }
         $academicYear = $this->academicYearRepository->findById($id);
 
         if ($academicYear) {
@@ -42,72 +60,103 @@ class AcademicYearController extends BaseController
             return response()->json([
                'success' => false,
                'message' => 'Không tìm thấy niên khóa!',
-            ], 404);
+            ], 400);
         }
     }
     
-    public function store(AcademicYearRequest $request)
-    {
-        $dataInsert = [
-            'name' => $request->name,
-            'code' => AcademicYear::generateRandomCode(),
-            'start_year' => $request->start_year,
-            'end_year' => $request->end_year,
-            'status' => $request->status,
-            'created_user_id' => $request->created_user_id, 
-            'modified_user_id' => $request->modified_user_id, 
-            'created_at' => now(),
-        ];
-
-        $item = $this->academicYearRepository->create($dataInsert);
-
-        if ($item) {
-            return response()->json([
-                'success' => true,
-                'message' => 'Thêm thành công!',
-                'data' => $item,
-            ], 201);
-        } else {
-            return response()->json([
-                'success' => false,
-                'message' => 'Thêm thất bại!',
-            ], 500);
-        }
+    public function store(AcademicYearRequest $request, GetUserRepository $getUserRepository)
+{
+    $user_id = $request->user_id;
+    $type = AccessTypeEnum::MANAGER->value;
+    
+    $createdUser = $getUserRepository->getUser($user_id, $type); 
+    if (!$createdUser) {
+        return $this->responseError(trans('api.error.user_not_permission'));
     }
 
+    $dataInsert = [
+        'name' => $request->name,
+        'code' => AcademicYear::generateRandomCode(),
+        'start_year' => $request->start_year,
+        'end_year' => $request->end_year,
+        'status' => $request->status,
+        'created_user_id' => $user_id,
+        'created_at' => now(),
+    ];
 
-public function update(AcademicYearRequest $request, $id)
+    
+    $item = $this->academicYearRepository->create($dataInsert);
+
+    if ($item) {
+        return response()->json([
+            'success' => true,
+            'message' => 'Thêm niên khóa thành công!',
+            'data' => $item,
+        ], 201);
+    } else {
+        return response()->json([
+            'success' => false,
+            'message' => 'Thêm niên khóa thất bại!',
+        ], 400);
+    }
+}
+
+
+
+public function update(AcademicYearRequest $request, int $id, GetUserRepository $getUserRepository)
     {
+        $user_id = $request->user_id;
+        $type = AccessTypeEnum::MANAGER->value;
+        
+        $modifiedUser = $getUserRepository->getUser($user_id, $type); 
+        if (!$modifiedUser) {
+            return $this->responseError(trans('api.error.user_not_permission'));
+        }
         $dataUpdate = [
             'name' => $request->name,
-            'start_year' => $request->start_year,
-            'end_year' => $request->end_year,
             'status' => $request->status,
-            'modified_user_id' => $request->modified_user_id, 
+            'start_year'=> $request->start_year,
+            'end_year'=> $request->end_year,
+            'modified_user_id' => $user_id,
             'updated_at' => now(),
         ];
 
+        
         $item = $this->academicYearRepository->update($id, $dataUpdate);
 
         if ($item) {
             return response()->json([
                 'success' => true,
-                'message' => 'Cập nhật thành công!',
+                'message' => 'Cập nhật niên khóa thành công!',
                 'data' => $item,
             ], 200);
         } else {
             return response()->json([
                 'success' => false,
-                'message' => 'Cập nhật thất bại!',
-            ], 500);
+                'message' => 'Niên khóa không tồn tại',
+            ], 400);
         }
     }
 
 
-    public function delete($id)
+    public function delete(Request $request, int $id, GetUserRepository $getUserRepository)
     {
-        $deletedAcademicYear = $this->academicYearRepository->softDelete($id);
         
+        $user_id = $request->user_id;
+        
+        $type = AccessTypeEnum::MANAGER->value;
+    
+        
+        $deleteUser = $getUserRepository->getUser($user_id, $type); 
+        if (!$deleteUser) {
+            
+            return $this->responseError(trans('api.error.user_not_permission'));
+        }
+    
+        // Thực hiện xóa mềm với id niên khóa và user_id
+        $deletedAcademicYear = $this->academicYearRepository->softDelete($id, $user_id);
+    
+        // Nếu xóa thành công
         if ($deletedAcademicYear) {
             return response()->json([
                 'success' => true,
@@ -116,11 +165,14 @@ public function update(AcademicYearRequest $request, $id)
             ], 200);
         }
     
+        // Nếu không thành công, trả về lỗi
         return response()->json([
             'success' => false,
-            'message' => 'Không tìm thấy niên khóa hoặc niên khóa vẫn còn hoạt động',
-        ], 403);
+            'message' => 'Niên khóa không tôn tại hoặc vẫn còn hoạt động!',
+        ], 400); 
     }
+    
+
     
 
 }
--- app/Domain/AcademicYear/Repository/AcademicYearReposity.php
+++ app/Domain/AcademicYear/Repository/AcademicYearReposity.php
@@ -1,5 +1,9 @@
 <?php
 namespace App\Domain\AcademicYear\Repository;
+
+use App\Common\Enums\DeleteEnum;
+use App\Common\Enums\StatusAcademicEnum;
+use App\Common\Enums\StatusEnum;
 use App\Domain\AcademicYear\Requests\AcademicYearRequest;
 use App\Domain\AcademicYear\Models\AcademicYear;
 class AcademicYearReposity {
@@ -9,7 +13,7 @@ class AcademicYearReposity {
 
     public function getAcademicYear()
 {
-    return AcademicYear::where('is_deleted', 0)->get();
+    return AcademicYear::where('is_deleted', DeleteEnum::NOT_DELETE->value)->get();
 }
 
 
@@ -24,7 +28,7 @@ public function create(array $data)
 
 public function findById(int $id)
 {
-    return AcademicYear::find($id);
+    return AcademicYear::where('is_deleted',DeleteEnum::NOT_DELETE->value)->find($id);
 }
 
 
@@ -44,19 +48,25 @@ public function update(int $id, array $data)
 }
 
 
-public function softDelete(int $id)
+public function softDelete(int $id, int $user_id)
 {
+   
     $academicYear = AcademicYear::find($id);
 
     
-    if ($academicYear && ($academicYear->status == 1 || $academicYear->status == 2)) {
-        $academicYear->is_deleted = 1; 
+    if ($academicYear && ($academicYear->status == 0 || $academicYear->status == 2)) {
+        
+        $academicYear->is_deleted = DeleteEnum::DELETED->value;
+        
+        $academicYear->modified_user_id = $user_id;
+        
         $academicYear->save();
         
-        return $academicYear;
+        return $academicYear; 
     }
 
-    return null;
+    return null; 
 }
+
 
 }
