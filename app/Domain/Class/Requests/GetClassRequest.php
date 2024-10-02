9ac3989134d4092be70e77ea2c35e1b2a761f28d
huythq <huythq@omt.vn>
2024-10-02 23:39:28 +0700

    commit

--- app/Domain/Class/Requests/GetClassRequest.php
+++ app/Domain/Class/Requests/GetClassRequest.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Domain\Class\Requests;
 
+use App\Http\Requests\BaseRequest;
 use Illuminate\Foundation\Http\FormRequest;
 
-class GetClassRequest extends FormRequest
+class GetClassRequest extends BaseRequest
 {
     public function __construct()
     {
@@ -12,6 +13,10 @@ class GetClassRequest extends FormRequest
     public function rules(): array
     {
         return [
+            "school_year_id" => "required|integer|exists:school_year,id",
+            "page"   => "integer|nullable",
+            "size"   => "integer|nullable",
+            "search" => "string|nullable",
         ];
     }
     
