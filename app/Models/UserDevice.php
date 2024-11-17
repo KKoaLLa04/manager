b15a5747ba81b7360651d461c679473789815ad8
quanghuy <huyp3172004@gmail.com>
2024-11-17 20:11:00 +0700

    noti

--- app/Models/UserDevice.php
+++ app/Models/UserDevice.php
@@ -5,19 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Grades extends Model
+class UserDevice extends Model
 {
     use HasFactory;
-
-    protected $table = 'grades';
+    protected $table = 'user_device';
 
     protected $fillable = [
         'id',
-        'name',
+        'user_id',
+        'device_token',
+        'device_type',
+        'status',
+        'created_at',
+        'updated_at',
     ];
-
-    public function classes()
-    {
-        return $this->hasMany(Classes::class, 'grade_id');
-    }
 }
