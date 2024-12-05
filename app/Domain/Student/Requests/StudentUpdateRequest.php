05eb83d4a8eb3a1af87b6771c897e5544d060bf2
xuanhoang2004 <xuanhoangg2004@gmail.com>
2024-12-05 22:26:40 +0700

    update code student vs rollcallhistory

--- app/Domain/Student/Requests/StudentUpdateRequest.php
+++ app/Domain/Student/Requests/StudentUpdateRequest.php
@@ -15,24 +15,15 @@ class StudentUpdateRequest extends FormRequest
     {
         return [
             'fullname' => 'required|min:3|max:255',
-            'dob' => 'required|date|before:today',
+            'dob' => 'date|before:today|nullable',
             'status' => 'required|in:0,1,2',
             'gender' => 'required|in:1,2',
             'class_id' => [
                     'nullable',
                     'integer',
-                    'exists:classes,id',
-                    'required_if:status,' . StatusClassStudentEnum::STUDYING->value,
-                    function ($attribute, $value, $fail) {
-                        // Lấy giá trị status từ request
-                        $status = $this->input('status');
-                        
-                        if (($status == StatusClassStudentEnum::STUDYING->value || $status == StatusClassStudentEnum::LEAVE->value) 
-                        && $this->input('status') == StatusClassStudentEnum::NOT_YET_CLASS->value) {
-                        $fail('Không thể chuyển sang trạng thái chưa vào lớp khi học sinh đã có lớp.');
-                        }
-                    
-                    },
+                  
+               
+                   
                 ],
         ];
     }
@@ -41,7 +32,6 @@ class StudentUpdateRequest extends FormRequest
     {
         return [
             'fullname.required' => 'Họ tên là bắt buộc.',
-            'dob.required' => 'Ngày sinh là bắt buộc.',
             'dob.before' => 'Ngày sinh phải nhỏ hơn ngày hiện tại.',
             'status.required' => 'Trạng thái là bắt buộc.',
             'status.in' => 'Trạng thái không hợp lệ.',
