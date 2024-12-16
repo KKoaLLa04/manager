234e42d6df42c12a8223cd389068dda65dff362c
TheBao2004 <nguyentheb705@gmail.com>
2024-12-17 02:41:26 +0700

    Sua code danh sach nhan vien

--- app/Domain/User/Repository/UserIndexRepository.php
+++ app/Domain/User/Repository/UserIndexRepository.php
@@ -10,10 +10,7 @@ class UserIndexRepository {
 
     public function handle ($keyword = "") {
 
-        $list = User::where('is_deleted', DeleteEnum::NOT_DELETE->value)->orWhere(function ($query) {
-            $query->where('access_type', AccessTypeEnum::MANAGER->value)
-                  ->where('access_type', AccessTypeEnum::TEACHER->value);
-        })->where("fullname", "like", "%".$keyword."%")->get();
+        $list = User::where('is_deleted', DeleteEnum::NOT_DELETE->value)->whereIn('access_type', [AccessTypeEnum::MANAGER->value, AccessTypeEnum::TEACHER->value])->where("fullname", "like", "%".$keyword."%")->get();
 
         if($list->count() > 0){
             return $list->map(function ($item) {
