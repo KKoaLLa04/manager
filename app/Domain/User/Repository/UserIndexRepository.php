841cfe1c27ceaefb5f2754611b255276bb85c679
BigH0405 <khanhbeotixiu8@gmail.com>
2024-12-17 03:05:17 +0700

    Search code cong nhan vien chuc

--- app/Domain/User/Repository/UserIndexRepository.php
+++ app/Domain/User/Repository/UserIndexRepository.php
@@ -10,7 +10,7 @@ class UserIndexRepository {
 
     public function handle ($keyword = "") {
 
-        $list = User::where('is_deleted', DeleteEnum::NOT_DELETE->value)->whereIn('access_type', [AccessTypeEnum::MANAGER->value, AccessTypeEnum::TEACHER->value])->where("fullname", "like", "%".$keyword."%")->get();
+        $list = User::where('is_deleted', DeleteEnum::NOT_DELETE->value)->whereIn('access_type', [AccessTypeEnum::MANAGER->value, AccessTypeEnum::TEACHER->value])->where("fullname", "like", "%".$keyword."%")->orWhere("code", "like", "%".$keyword."%")->get();
 
         if($list->count() > 0){
             return $list->map(function ($item) {
