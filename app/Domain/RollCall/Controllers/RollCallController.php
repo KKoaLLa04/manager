61a4058ff24749d29c17e8b1734520b71099edb9
quanghuy <huyp3172004@gmail.com>
2024-12-17 22:30:26 +0700

    dev

--- app/Domain/RollCall/Controllers/RollCallController.php
+++ app/Domain/RollCall/Controllers/RollCallController.php
@@ -25,10 +25,10 @@ class RollCallController extends BaseController
 
     protected $rollCallRepository;
 
-    public function __construct(RollCallRepository $rollCallRepository,
-        protected  ClassRepository $classRepository,
-    )
-    {
+    public function __construct(
+        RollCallRepository        $rollCallRepository,
+        protected ClassRepository $classRepository,
+    ) {
         $this->rollCallRepository = $rollCallRepository;
     }
 
@@ -42,8 +42,8 @@ class RollCallController extends BaseController
             return $this->responseError(trans('api.error.user_not_permission'));
         }
 
-
-        $day      = Carbon::now()->dayOfWeek;
+        $date     = isset($date) ? Carbon::parse($date) : Carbon::now();
+        $day      = $date->dayOfWeek;
         $dayQuery = 0;
         switch ($day) {
             case 1:
@@ -79,33 +79,35 @@ class RollCallController extends BaseController
         $classes      = Classes::query()->whereIn('id', $classIds)->where('status',
             StatusEnum::ACTIVE->value)->where('is_deleted', DeleteEnum::NOT_DELETE->value)->get();
         $timetableIds = $timetables->pluck('id')->toArray();
-        $rollCalls  = RollCall::query()->whereIn('diemdanh_id', $timetableIds)->get()->groupBy('diemdanh_id');
-        $timetables = $timetables->groupBy('class_id');
-        $data       = [];
+        $rollCalls    = RollCall::query()->whereIn('diemdanh_id', $timetableIds)->where('date',
+            $date->toDateString())->get()->groupBy('diemdanh_id');
+        $timetables   = $timetables->groupBy('class_id');
+        $data         = [];
         foreach ($timetables as $key => $timetable) {
-            $class           = $classes->where('id', $key)->first();
+            $class      = $classes->where('id', $key)->first();
             $data[$key] = [
                 'ClassId'   => $class->id,
                 'ClassName' => $class->name,
             ];
-            $timetable       = $timetable->groupBy('buoi');
+            $timetable  = $timetable->groupBy('buoi');
             foreach ($timetable as $keytime => $item) {
                 $data[$key]['timetable'][$keytime] = [
-                    $item->map(function ($item) use ($rollCalls,$class) {
+                    $item->map(function ($item) use ($rollCalls, $class) {
                         $rollCall = $rollCalls->get($item->id);
 
                         return [
-                            "idTimetable" => $item->id,
-                            "checkAttendance" => $this->rollCallRepository->checkAttendanceLog($class->id, $item->id) ? 1 : 0,
-                            "userId"      => is_null($item->classSubjectTeacher) ? 0 : $item->classSubjectTeacher->teacher->id,
-                            "fullname"    => is_null($item->classSubjectTeacher) ? 0 : $item->classSubjectTeacher->teacher->fullname ?? "",
-                            "email"       => is_null($item->classSubjectTeacher) ? 0 : $item->classSubjectTeacher->teacher->email ?? "",
-                            "rollcall"    => is_null($rollCall) ? [
+                            "idTimetable"     => $item->id,
+                            "checkAttendance" => $this->rollCallRepository->checkAttendanceLog($class->id,
+                                $item->id) ? 1 : 0,
+                            "userId"          => is_null($item->classSubjectTeacher) ? 0 : $item->classSubjectTeacher->teacher->id,
+                            "fullname"        => is_null($item->classSubjectTeacher) ? 0 : $item->classSubjectTeacher->teacher->fullname ?? "",
+                            "email"           => is_null($item->classSubjectTeacher) ? 0 : $item->classSubjectTeacher->teacher->email ?? "",
+                            "rollcall"        => is_null($rollCall) ? [
                                 'totalRollCall' => 0,
-                                'totalStudent' => $this->classRepository->getStudentOfClass($class->id)->count(),
+                                'totalStudent'  => $this->classRepository->getStudentOfClass($class->id)->count(),
                             ] : [
                                 'totalRollCall' => $rollCall->count(),
-                                'totalStudent' => $this->classRepository->getStudentOfClass($class->id)->count(),
+                                'totalStudent'  => $this->classRepository->getStudentOfClass($class->id)->count(),
                             ]
 
                         ];
@@ -117,14 +119,14 @@ class RollCallController extends BaseController
     }
 
 
-    public function studentInClass($class_id,$diemdanh_id, Request $request)
+    public function studentInClass($class_id, $diemdanh_id, Request $request)
     {
         // Lấy tham số name và student_code từ request
         $name         = $request->input('name', null);         // Tên học sinh
         $student_code = $request->input('student_code', null); // Mã học sinh
 
         // Gọi repository để lấy danh sách học sinh theo lớp và tham số tìm kiếm
-        $student = $this->rollCallRepository->getStudent($class_id,$diemdanh_id, $name, $student_code);
+        $student = $this->rollCallRepository->getStudent($class_id, $diemdanh_id, $name, $student_code);
         // Kiểm tra và trả về kết quả
         if ($student) {
             return $this->responseSuccess($student, trans('api.rollcall.index.success'));
@@ -152,8 +154,8 @@ class RollCallController extends BaseController
 
         $rollCallData = $request->input('rollcallData', []);
         $date         = isset($request->date) ? Carbon::parse($request->date) : now();
-        $diemdanhId = $request->diemdanh_id;
-        $this->rollCallRepository->attendanceStudentOfClass($diemdanhId,$classId, $rollCallData, $user_id, $date);
+        $diemdanhId   = $request->diemdanh_id;
+        $this->rollCallRepository->attendanceStudentOfClass($diemdanhId, $classId, $rollCallData, $user_id, $date);
 
         return $this->responseSuccess([], trans('api.rollcall.attendaced.success'));
     }
