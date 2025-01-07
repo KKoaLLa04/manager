f275bc48383e8ebcf780dab7f95ebdf2ef194d86
bigh0405 <khanhbeotixiu8@gmail.com>
2025-01-07 23:23:04 +0700

    fix bug phu huynh

--- app/Domain/Guardian/Requests/GuardianLayoutTeacherRequest.php
+++ app/Domain/Guardian/Requests/GuardianLayoutTeacherRequest.php
@@ -1,35 +1,37 @@
 <?php
+
 namespace App\Domain\Guardian\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class GuardianLayoutTeacherRequest extends FormRequest
 {
-    public function __construct()
-    {
-    }
+    public function __construct() {}
 
     // Quy tắc xác thực
     public function rules(): array
-{
-    $rules = [
-        'fullname' => ['required'],
-        'dob' => ['nullable','timestamp'],
-        'phone' => ['regex:/^0[0-9]{9}$/'],
-        'status' => ['required', 'integer'],
-        'gender' => ['required', 'integer'],
-        'email' => ['email', 'unique:users,email'. $this->route('id')],
-    ];
+    {
+        $rules = [
+            'fullname' => ['required'],
+            'dob' => ['nullable', 'date'],
+            'phone' => ['regex:/^0[0-9]{9}$/'],
+            'status' => ['required', 'integer'],
+            'gender' => ['required', 'integer'],
+            'email' => [
+                'nullable',
+                'email',
+                Rule::unique('users', 'email')->ignore($this->route('id')),
+            ],
+        ];
 
-    return $rules;
-}
+        return $rules;
+    }
 
 
     // Thông báo lỗi
     public function messages(): array
     {
-        return [
-
-        ];
+        return [];
     }
 }
