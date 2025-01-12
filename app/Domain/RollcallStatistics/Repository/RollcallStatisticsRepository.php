afcb43903741509e92c9bf6259575805cd10a380
quanghuy <huyp3172004@gmail.com>
2025-01-13 00:25:33 +0700

    time

--- app/Domain/RollcallStatistics/Repository/RollcallStatisticsRepository.php
+++ app/Domain/RollcallStatistics/Repository/RollcallStatisticsRepository.php
@@ -180,9 +180,9 @@ class RollcallStatisticsRepository
                             ->first();
                         return [
                             'period'       => $timetable->period,
-                            'subject_name' => is_null($teacherSubjectTimetable->subject) ? $teacherSubjectTimetable->subject->name : "",
-                            'user_name'    => is_null($rollCall) ? $rollCall->createdUser->fullname : "",
-                            'time'         => is_null($rollCall) ? $rollCall->time : "",
+                            'subject_name' => !is_null($teacherSubjectTimetable->subject) ? $teacherSubjectTimetable->subject->name : "",
+                            'user_name'    => !is_null($rollCall) ? $rollCall->createdUser->fullname : "",
+                            'time'         => !is_null($rollCall) ? $rollCall->time : "",
                             'status'       => is_null($rollCall) ? StatusStudentEnum::HOLIDAY->value : $rollCall->status,
                         ];
                     })->toArray();
