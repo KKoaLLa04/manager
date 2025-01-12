5e3fcfe0856ec37579e3735e95ad58935c8897b5
bigh0405 <khanhbeotixiu8@gmail.com>
2025-01-12 14:06:39 +0700

    Code lich su diem danh staff va teacher

--- app/Domain/RollCallHistory/Routes/routes.php
+++ app/Domain/RollCallHistory/Routes/routes.php
@@ -2,11 +2,19 @@
 <?php
 
 use App\Domain\RollCallHistory\Controllers\RollCallHistoryController;
+use App\Domain\RollCallHistory\Controllers\RollCallHistoryTeacherController;
+use App\Domain\RollCallTeacher\Controllers\RollCallTeacherController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'manager/rollcallhistory', 'middleware' => 'auth:api'], function () {
 
     Route::get('/', [RollCallHistoryController::class, 'index']);
     Route::get('showclass/{classId}', [RollCallHistoryController::class, 'showRollCallHistories']);
-    Route::get('class/{classId}/roll-call-history/{timetableId}/{date}', [RollCallHistoryController::class, 'showRollCallHistoryDetails']);
+    Route::get('student/{class_id}/{teacher_subject_timetable_id}', [RollCallHistoryController::class, 'studentInClass']);
+});
+
+Route::group(['prefix' => 'teacher/rollcallhistory', 'middleware' => 'auth:api'], function () {
+    Route::get('/', [RollCallHistoryTeacherController::class, 'getClass']);
+    Route::get('showclass/{classId}', [RollCallHistoryTeacherController::class, 'showRollCallHistories']);
+    Route::get('student/{class_id}/{teacher_subject_timetable_id}', [RollCallHistoryTeacherController::class, 'studentInClass']);
 });
